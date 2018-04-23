0312dd8f35ecb335bc7b3f74a88ef21fdfb8b400
l-13 <211530160@qq.com>
2018-04-23 15:05:58 +0800

    xxx

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,54 @@
 <template>
   <div class="ws-content">
+
+
+    <!-- 头部 -->
     <div class="navbar">
-      <h1 class="nav-bar-title">首页<small></small></h1>
+      <h1 class="nav-bar-title">信息<small> | 基础信息</small></h1>
     </div>
+    <div class="databox">
+      <el-row type="flex" class="row-bg" justify="space-around">
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            <span>用户数量</span>
+            <div class="listLift">
+              <span class="listTitle">123</span>
+            </div>
+            <div class="listRight">
+              <img src="../../static/img/userNum.png" alt="">
+            </div>
+            <span class="listString"></span>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple-light">
+            <span class="listTitle">当前会员</span>
+            <div class="listLift">
+              <span>123</span>
+            </div>
+            <div class="listRight">
+              <img src="../../static/img/currenVip.png" alt="">
+            </div>
+            <span class="listString"></span>
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+            <span class="listTitle">当前订单数量</span>
+            <div class="listLift">
+              <span>123</span>
+            </div>
+            <div class="listRight">
+              <img src="../../static/img/currenOrderNum.png" alt="">
+            </div>
+            <span class="listString"></span>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+
+
+    <!--  -->
   </div>
 </template>
 <script>
@@ -19,5 +65,10 @@ export default {
 </script>
 
 <style scoped>
+  .grid-content{
+    border: 1px solid red;
+  }
+  .listTitle{
 
+  }
 </style>
